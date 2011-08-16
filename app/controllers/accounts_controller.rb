88b559bde9373d90bc5a220614e9bafccf550181
John Wilger <johnwilger@gmail.com>
2011-08-16 14:56:53 -0700

    Developer: Implemented account creation

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,9 +1,17 @@
 class AccountsController < ApplicationController
   attr_reader :account
 
-  skip_before_filter :require_authenticated_user, :only => [:new]
+  skip_before_filter :require_authenticated_user, :only => [:new, :create]
 
   def new
     @account = Account.new
+  end
+
+  def create
+    self.current_account = Account.create!(params[:account])
+    redirect_to todo_list_path
+  rescue ActiveRecord::RecordInvalid => e
+    @account = e.record
+    render :action => :new, :status => 403
   end
 end
