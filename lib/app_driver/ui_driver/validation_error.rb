ea33efb84cbac33b840cbac9c8904cc59700c8f0
John Wilger <johnwilger@gmail.com>
2011-08-17 21:20:23 -0700

    QA: update ui components to use a css locator to determine visibility

--- lib/app_driver/ui_driver/validation_error.rb
+++ lib/app_driver/ui_driver/validation_error.rb
@@ -3,7 +3,9 @@ require 'app_driver/ui_driver/ui_component'
 class AppDriver::UIDriver::ValidationError
   include AppDriver::UIDriver::UIComponent
 
-  def _visible?
-    browser.page.has_css?('#form_validation_error')
+  private
+
+  def component_locator
+    '#form_validation_error'
   end
 end
